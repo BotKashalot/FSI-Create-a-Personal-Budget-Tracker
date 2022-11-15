4d7a88843879b2c8ff046483e566ef0eafa45786
Tzvetomir Kotzev <tzvetomirkotzev@tzvetomirs-air.home>
2022-11-15 10:42:36 -0500

    personal Budget Bonus 2 while loops

--- index.js
+++ index.js
@@ -39,17 +39,25 @@ let annualExpenses = 0
 
 
 
-for(let i = weeklyExpenseQuestions.length; i > 0; i--){
-    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+//for(let i = weeklyExpenseQuestions.length; i > 0; i--){
+//    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+//    weeklyExpenses = weeklyExpenses + answer
+//}
+//
+//for(let i = weeklyExpenseQuestions.length; i > 0; i--){
+//    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+//    monthlyExpenses = monthlyExpenses + answer
+//}
+//
+//for(let i = weeklyExpenseQuestions.length; i > 0; i--){
+//    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+//    annualExpenses = annualExpenses + answer
+//}
+
+let n = 0;
+
+while (n < weeklyExpenseQuestions.length){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[n]))
     weeklyExpenses = weeklyExpenses + answer
-}
-
-for(let i = weeklyExpenseQuestions.length; i > 0; i--){
-    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
-    monthlyExpenses = monthlyExpenses + answer
-}
-
-for(let i = weeklyExpenseQuestions.length; i > 0; i--){
-    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
-    annualExpenses = annualExpenses + answer
+    n++
 }
